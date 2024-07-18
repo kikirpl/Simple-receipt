f77a7f56919c5cef451ddbf259f5cb886cf8f3bb
kikirpl <rizkyibrohim88@gmail.com>
2024-07-18 11:03:16 +0700

    first

---
+++ script.js
@@ -0,0 +1,46 @@
+let BtnSubmit = document.getElementById("btnSubmit");
+let carts = [];
+
+function submitHandler() {
+  let id;
+  if (carts.length === 0) {
+    id = 1;
+  } else {
+    id = carts[carts.length - 1].id + 1;
+  }
+  let barangValue = document.getElementById("barang").value;
+  let hargaValue = document.getElementById("harga").value;
+  let namaValue = document.getElementById("nama").value;
+
+  let tempObj = {
+    id,
+    nama: barangValue,
+    harga: +hargaValue,
+  };
+  document.getElementById("namaCart").innerHTML = namaValue;
+  carts.push(tempObj);
+  getCarts();
+  console.log(carts);
+}
+
+function getCarts() {
+  let tBody = document.getElementById("tBody");
+  tBody.innerHTML = "";
+  carts.forEach((cart) => {
+    tBody.innerHTML += `<tr>
+    <td> ${cart.id}</td>
+    <td> ${cart.nama}</td>
+    <td> ${cart.harga}</td>
+    </tr>`;
+  });
+}
+let btnPrint = document.getElementById("btnPrint");
+BtnSubmit.addEventListener("click", submitHandler);
+
+function printHandler() {
+  document.querySelector(".form-box").style.display = "none";
+  btnPrint.style.display = "none";
+  window.print();
+}
+
+btnPrint.addEventListener("click", printHandler);
